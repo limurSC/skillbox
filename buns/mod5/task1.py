177ca76e3fa3101a833ad471e5cda91a07f8d1b5
Ilnur <128057310+limurSC@users.noreply.github.com>
2023-10-26 15:02:42 +0300

    Update task1.py

--- buns/mod5/task1.py
+++ buns/mod5/task1.py
@@ -1,1 +1,36 @@
-s=
+class Node:
+    """
+    Вспомогательный класс для узлов списка
+    """
+    def __init__(self, data):
+        self.data = data  # храним информацию
+        self.pref = None  # ссылка на предыдущий узел
+
+class Stack:
+    """
+    Основной класс для стека
+    """
+
+    def __init__(self):
+        self.end = None  # ссылка на конец стека
+
+    def pop(self):
+        """
+        возвращение последнего элемента из списка с удалением его из списка
+        """
+        if self.end == None:
+        	pass
+        return val
+
+    def push(self, val):
+        """
+        добавление элемента val в конец списка
+        """
+        pass
+
+    def print(self):
+        """
+        вывод на печать содержимого стека
+        """
+        
+        pass
