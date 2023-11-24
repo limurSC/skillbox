7c18cf8baab761d8c270c33e8860e2073a4ce70f
limurSC <0709110176ilnur@gmail.com>
2023-11-24 17:47:14 +0500

    mod10

---
+++ pract/mod10/task1.py
@@ -0,0 +1,38 @@
+import re
+import doctest
+
+def check_password(password):
+    """
+    >>> check_password("rtG&3FG#Tr^e")
+    True
+    >>> check_password("a^A1@*@1Aa")
+    True
+    >>> check_password("oF^a1D@y5%e6")
+    True
+    >>> check_password("enroi#$*rkdeR#$*092uwedchf34tguv394h")
+    True
+    >>> check_password("пароль")
+    False
+    >>> check_password("password")
+    False
+    >>> check_password("qwerty")
+    False
+    >>> check_password("lOngPa$$>W0Rd")
+    False
+    >>> check_password("a^A1@*@1Aa!")
+    False
+    >>> check_password(124125432)
+    Traceback (most recent call last):
+        ...
+    Exception
+    """
+    if not isinstance(password, str):
+        raise Exception
+    if re.findall(r"^(?!.*[,.!?])^(?=.*[a-z].*[a-z])(?=.*\d)(?=(?:[^$^%@#&*,.!?]*[$^%@#&*]){3})[A-Za-z0-9^$%@#&*]{8,}$",
+                  password):
+        return True
+    else:
+        return False
+
+
+doctest.testmod(verbose=True)
